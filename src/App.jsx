2b2c1e713924eda718beb3c9a3ec8a2398e8c4c3
Johanna Winter <johanna.winter@gmx.de>
2025-11-03 14:40:26 +0100

    add ColorForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import { useState } from "react";
+import ColorForm from "./Components/Color/ColorForm";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+  function handleAddColor(newColor) {
+    console.log("New color: ", newColor);
+    setColors([newColor, ...colors]);
+  }
   return (
     <>
       <h1>Theme Creator</h1>
-
+      <ColorForm onAddColor={handleAddColor} />
       {initialColors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
--- src/Components/Color/ColorForm.jsx
+++ src/Components/Color/ColorForm.jsx
@@ -1,7 +1,40 @@
-import { useState } from "react";
-import { initialColors } from "../../lib/colors";
+import "./ColorForm.css";
 
-export default function ColorForm({ onAddColor }) {
-  const [color, setColor] = useState({ defaultValue: initialColors });
-  return;
+export default function ColorForm({ newColor, onAddColor }) {
+  function handleAddColor(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onAddColor(data);
+    // console.log("Does everything work? ", data);
+    event.target.reset();
+  }
+  return (
+    <form className="color-form" onSubmit={handleAddColor}>
+      <label htmlFor="color-role">Role:</label>
+      <input
+        id="color-role"
+        type="text"
+        defaultValue="primary main"
+        name="role"
+      ></input>
+      <label htmlFor="color-hex">Hex Value:</label>
+      <input
+        id="color-hex"
+        type="color"
+        defaultValue="#802922"
+        name="hex"
+      ></input>
+      <label htmlFor="color-contrast-text">Contrast Text:</label>
+      <input
+        id="color-contrast-text"
+        type="color"
+        defaultValue="#FFFFFF"
+        name="contrastText"
+      ></input>
+      <button className="color-form__button" type="submit">
+        Add color
+      </button>
+    </form>
+  );
 }
